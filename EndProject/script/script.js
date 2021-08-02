20c4a72affc30d976d9021ee7d6f6ff7651935f1
mrmamedzade <cafarmm@code.edu.az>
2021-08-03 01:41:21 +0400

    navbar responsibility is done

--- EndProject/script/script.js
+++ EndProject/script/script.js
@@ -1,14 +1,13 @@
-const hamburger = document.getElementById('hamburger');
-const navUL = document.getElementById('nav-ul');
-
-hamburger.addEventListener('click',() => {
-    navUL.classList.toggle('show');
-});
+ 
 
 
-const browse = document.getElementById('browse');
-const browseUL = document.getElementById('browseul');
 
-    browse.addEventListener('click',() =>{
-        browseUL.classList.toggle('show');
-    });
+var navLinks=document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.display="block";
+}
+
+function hideMenu(){
+    navLinks.style.display="none";
+}
