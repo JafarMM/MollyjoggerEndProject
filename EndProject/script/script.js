aad748f6e92ca24d7924f97452b0ba01ca87572a
mrmamedzade <cafarmm@code.edu.az>
2021-08-02 13:08:05 +0400

    Browse mobile responsive is done.

--- EndProject/script/script.js
+++ EndProject/script/script.js
@@ -4,3 +4,11 @@ const navUL = document.getElementById('nav-ul');
 hamburger.addEventListener('click',() => {
     navUL.classList.toggle('show');
 });
+
+
+const browse = document.getElementById('browse');
+const browseUL = document.getElementById('browseul');
+
+    browse.addEventListener('click',() =>{
+        browseUL.classList.toggle('show');
+    });
