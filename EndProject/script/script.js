33ee3f55242666c05b8d865616c6f3b8a4e44da5
mrmamedzade <cafarmm@code.edu.az>
2021-08-05 02:26:04 +0400

    product detail page is done

--- EndProject/script/script.js
+++ EndProject/script/script.js
@@ -5,9 +5,26 @@
 var navLinks=document.getElementById("navLinks");
 
 function showMenu(){
-    navLinks.style.display="block";
+    navLinks.style.right="0%";
 }
 
 function hideMenu(){
-    navLinks.style.display="none";
+    navLinks.style.right="-100%";
+}
+
+
+var MainImg=document.getElementById("MainImg");
+var smallimg=document.getElementsByClassName('small-img');
+
+smallimg[0].onclick=function(){
+    MainImg.src=smallimg[0].src;
+}
+smallimg[1].onclick=function(){
+    MainImg.src=smallimg[1].src;
+}
+smallimg[2].onclick=function(){
+    MainImg.src=smallimg[2].src;
+}
+smallimg[3].onclick=function(){
+    MainImg.src=smallimg[3].src;
 }
